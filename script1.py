42e3277a3d1d24d590d5a788429dd425d0e85f10
gsingh283 <156484481+gsingh283@users.noreply.github.com>
2024-01-16 11:09:06 -0500

    Modifed both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added one print statement in the script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from the zen of pyhton
 print('Flat is better than nested.')
-print('Explicit is better than implicit.')
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from the zen of pyhton
 print('Beautiful is better than ugly.')
 print('Simple is better than complex.')
+print('Explicit is better than implicit.')
