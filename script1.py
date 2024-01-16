59ee614b07698aa3d1f2b52daddc7880eae35772
gsingh283 <156484481+gsingh283@users.noreply.github.com>
2024-01-16 10:48:41 -0500

    Revert "Modified both script files"
    This reverts commit dd8a82c189c7262ebba993caf68ef8d1416b56cf.

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 # Print lines from the zen of pyhton
 print('Flat is better than nested.')
+print('Explicit is better than implicit.')
--- script2.py
+++ script2.py
@@ -1,3 +1,2 @@
-# Print lines from the zen of pyhton
 print('Beautiful is better than ugly.')
 print('Simple is better than complex.')
